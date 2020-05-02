2827dea121d8b9853706ca9d0900f7a42f82b8b0
Max G <maxdotdotg@gmail.com>
2020-05-02 10:41:54 -0400

    add Readme, stock ini, and setup script

---
+++ README.md
@@ -0,0 +1,6 @@
+# ini tweaks for XCOM Enemy Unknown
+
+A tweaked INI file to help the game run better on a low-spec machine
+
+
+~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# backup the default version
+cp ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini
+~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini.bak
+
+# overwrite with the tweaked version
+cp XComEngine.ini ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini
