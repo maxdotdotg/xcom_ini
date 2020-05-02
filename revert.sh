92323e064f5362c621f3360b16410d9d9e35d8ad
Max G <maxdotdotg@gmail.com>
2020-05-02 11:19:57 -0400

    add new-line escape in revert.sh

--- revert.sh
+++ revert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # revert to the backup
-cp ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini.bak ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini
+cp ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini.bak \
+    ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini
