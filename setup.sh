32415ae0910656e086f87a798524e259eb7f23ac
Max G <maxdotdotg@gmail.com>
2020-05-02 11:19:01 -0400

    fix missing new-line escape in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # backup the default version
-cp ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini
-~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini.bak
+cp ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini \
+    ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini.bak
 
 # overwrite with the tweaked version
 cp XComEngine.ini-mod ~/.local/share/feral-interactive/XCOM/WritableFiles/XComEngine.ini
